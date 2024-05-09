d076eb76301ebf1da5e0e21557e26b45651b1e5a
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-05-09 17:04:20 -0400

    added tamm_report() in minimal working form

---
+++ R/tamm_report.R
@@ -0,0 +1,54 @@
+
+
+#' Generate report of figures from TAMM file
+#'
+#' @param tamm.path Absolute path to directory containing the tamm file. (hoping to streamline this at some point). Character atomic.
+#' @param tamm.name Name of tamm file (including .xlsx suffix). Character atomic
+#' @param clean Should the .qmd files used to make the report be deleted afterwards? Logical, defaults to `TRUE`. Set to `FALSE`
+#' to explore the .qmd files underlying the report.
+#'
+#' @return Nothing
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' tamm.path = "C:/Users/edwc1477/Documents/WDFW FRAM team work/NOF material/NOF 2024/NOF 2"
+#' tamm.name = "Chin2124.xslx"
+#' tamm_report(tamm.path = tamm.path, tamm.name = "Chin2124.xlsx")
+#' }
+tamm_report <- function(tamm.path, tamm.name,  clean = TRUE){
+  if(!is.character(tamm.name)){
+    cli::cli_abort("`tamm.name` must be character string of tamm file name (including .xlsx suffix).")
+  }
+  if(gsub(".*[.]", "", tamm.name) != "xlsx"){
+    cli::cli_abort("`tamm.name` must describe .xlsx file.")
+  }
+  if(!is.character(tamm.path)){
+    cli::cli_abort("`tamm.path` must character string of absolute path to directory containing TAMM. Try using `here` package to help.")
+  }
+  if(!is.logical(clean)){
+    cli::cli_abort("`clean` must be logical.")
+  }
+
+  ##identify path to children
+  qmd.path = system.file("tamm-visualizer.qmd", package = "TAMMsupport")
+  qmd.child.path = system.file("tamm-visualizer-child.qmd", package = "TAMMsupport")
+
+  ## copy qmd files
+  cli::cli_alert(paste0("Copying parameterized report .qmd files to `", tamm.path, "`."))
+  file.copy (c(qmd.path, qmd.child.path), tamm.path)
+
+  ## generate report
+  report.name = gsub("[.].*", "-Visualization.html", tamm.name)
+  cli::cli_alert(paste0("Generating report."))
+  quarto::quarto_render(paste0(tamm.path,"/tamm-visualizer.qmd"),
+                        execute_params = list(tamm.path = paste0(tamm.path, "/", tamm.name)))
+  file.rename(paste0(tamm.path,"/tamm-visualizer.html"),
+              paste0(tamm.path, "/", report.name))
+
+  if(clean){
+    cli::cli_alert("Deleting .qmd files.")
+    file.remove(paste0(tamm.path,"/", c("tamm-visualizer.qmd", "tamm-visualizer-child.qmd")))
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# TAMMsupport
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of TAMMsupport is to …
+
+## Installation
+
+You can install the development version of TAMMsupport like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Devnotes
+
+Moving functions out of NOF 2024 project.
+
+- tamm-visualizer.qmd and support
+
+Files to look at:
+
+- tamm-compare-functionalized.R
+- ecel-compare-sandbox.R
+
+Check imports – for now, just listed everything library’d in the .qmd,
+but that may ot be necessary.
