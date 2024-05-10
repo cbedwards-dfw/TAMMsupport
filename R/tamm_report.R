ba29688d205eb4c17f0645b9aa7a2ca341ead288
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-05-10 12:46:17 -0400

    Added summary table to report

--- R/tamm_report.R
+++ R/tamm_report.R
@@ -2,10 +2,11 @@
 
 #' Generate report of figures from TAMM file
 #'
-#' @param tamm.path Absolute path to directory containing the tamm file. (hoping to streamline this at some point). Character atomic.
 #' @param tamm.name Name of tamm file (including .xlsx suffix). Character atomic
-#' @param clean Should the .qmd files used to make the report be deleted afterwards? Logical, defaults to `TRUE`. Set to `FALSE`
+#' @param tamm.path Absolute path to directory containing the tamm file. `here::here()` can be useful in identifying appropriate path. Character atomic; defaults to current working directory.
+#' @param clean Should the intermediate .qmd files used to make the report be deleted afterwards? Logical, defaults to `TRUE`. Set to `FALSE`
 #' to explore the .qmd files underlying the report.
+#' @param overwrite Should the intermediate .qmd files or the final report be overwritten if those files already exist?; Logical, defaults to `TRUE`.
 #'
 #' @return Nothing
 #' @export
@@ -14,9 +15,9 @@
 #' \dontrun{
 #' tamm.path = "C:/Users/edwc1477/Documents/WDFW FRAM team work/NOF material/NOF 2024/NOF 2"
 #' tamm.name = "Chin2124.xslx"
-#' tamm_report(tamm.path = tamm.path, tamm.name = "Chin2124.xlsx")
+#' tamm_report(tamm.name = "Chin2124.xlsx", tamm.path = tamm.path)
 #' }
-tamm_report <- function(tamm.path, tamm.name,  clean = TRUE){
+tamm_report <- function(tamm.name, tamm.path = getwd(),  clean = TRUE, overwrite = TRUE){
   if(!is.character(tamm.name)){
     cli::cli_abort("`tamm.name` must be character string of tamm file name (including .xlsx suffix).")
   }
@@ -29,26 +30,31 @@ tamm_report <- function(tamm.path, tamm.name,  clean = TRUE){
   if(!is.logical(clean)){
     cli::cli_abort("`clean` must be logical.")
   }
+  if(!file.exists(paste0(tamm.name, "/", tamm.path))){
+    cli::cli_abort("TAMM file was not found. Check that `tamm.path` and `tamm.name` are correct.")
+  }
 
   ##identify path to children
   qmd.path = system.file("tamm-visualizer.qmd", package = "TAMMsupport")
   qmd.child.path = system.file("tamm-visualizer-child.qmd", package = "TAMMsupport")
+  ## generate report name
+  report.name = gsub("[.].*", "-Visualization.html", tamm.name)
 
   ## copy qmd files
   cli::cli_alert(paste0("Copying parameterized report .qmd files to `", tamm.path, "`."))
-  file.copy (c(qmd.path, qmd.child.path), tamm.path)
+  file.copy (c(qmd.path, qmd.child.path), tamm.path, overwrite = overwrite)
 
   ## generate report
-  report.name = gsub("[.].*", "-Visualization.html", tamm.name)
   cli::cli_alert(paste0("Generating report."))
   quarto::quarto_render(paste0(tamm.path,"/tamm-visualizer.qmd"),
-                        execute_params = list(tamm.path = paste0(tamm.path, "/", tamm.name)))
+                        execute_params = list(tamm.path = tamm.path, tamm.name = tamm.name))
   file.rename(paste0(tamm.path,"/tamm-visualizer.html"),
               paste0(tamm.path, "/", report.name))
 
   if(clean){
-    cli::cli_alert("Deleting .qmd files.")
+    cli::cli_alert("Deleting intermediate .qmd files.")
     file.remove(paste0(tamm.path,"/", c("tamm-visualizer.qmd", "tamm-visualizer-child.qmd")))
   }
 
+  cli::cli_alert("Finished!")
 }
--- README.md
+++ README.md
@@ -6,26 +6,44 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of TAMMsupport is to …
+The goal of TAMMsupport is to provide R-based tools for working with and
+interpreting Terminal Area Management Module files
+(<https://framverse.github.io/fram_doc/calcs_glossary.html#Terminal_Area_Management_Module_(TAMM)>).
 
 ## Installation
 
 You can install the development version of TAMMsupport like so:
 
 ``` r
-# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+devtools::install_github("cbedwards-dfw/TAMMsupport)    
 ```
 
-## Devnotes
+or
 
-Moving functions out of NOF 2024 project.
+``` r
+pak::pkg_install("cbedwards-dfw/TAMMsupport")
+```
 
-- tamm-visualizer.qmd and support
+## Quick guide
 
-Files to look at:
+At present, the only function is `tamm_report()`. When provided a tamm
+file name and the path to the directory (defaults to using the current
+working directory), this generates an html report summarizing key
+aspects of the TAMM. The report is given the same name as the TAMM with
+the suffix “-Visualization”.
+
+Under the hood, tamm_report() uses a template parameterized quarto
+report and a child quarto file to help automate the creation of tabs for
+each stock. To see the underlying quarto files used (e.g., to suggest
+additional edits or features), set argument `clean = FALSE` when calling
+`tamm_report()`. This will prevent the deletion of `tamm-visualizer.qmd`
+and `tamm-visualizer-child.qmd`, which will be present in the same
+folder as the TAMM and report.
+
+## Devnotes to self
+
+Still in the process of transfering experimental functions from NOF 2024
+project. Files to look at:
 
 - tamm-compare-functionalized.R
-- ecel-compare-sandbox.R
-
-Check imports – for now, just listed everything library’d in the .qmd,
-but that may ot be necessary.
+- excel-compare-sandbox.R
